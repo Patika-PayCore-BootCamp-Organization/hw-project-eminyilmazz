c55a39895b7bb832b5d14b720fa1538bc437eecd
Emin <eminyilmzz@gmail.com>
2022-01-29 23:19:05 +0300

    Created JPA entity fields

--- src/main/java/com/ecommorce/eservice/model/Product.java
+++ src/main/java/com/ecommorce/eservice/model/Product.java
@@ -1,7 +1,23 @@
 package com.ecommorce.eservice.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@NoArgsConstructor
+@Entity
 public class Product {
-    private Integer id;
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+
+    @NotBlank
+    @Column(name = "product_name")
     private String name;
+
+    @Column(name = "image_url", nullable = true)
     private String imageUrl;
 }
