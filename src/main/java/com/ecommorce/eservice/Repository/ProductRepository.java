a329372e8627e6539b7a6d7fcfbe6aab551f3408
Emin <eminyilmzz@gmail.com>
2022-01-29 22:10:46 +0300

    Small fix

--- src/main/java/com/ecommorce/eservice/Repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommorce.eservice.Repository;
-
-import com.ecommorce.eservice.Model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
