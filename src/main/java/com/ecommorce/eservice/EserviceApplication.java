08aad1edb668705f2c8c7c259ea2799f2e290407
Emin <eminyilmzz@gmail.com>
2022-01-31 21:41:46 +0300

    Removed User.java entity and moved it to Customer.java

--- src/main/java/com/ecommorce/eservice/EserviceApplication.java
+++ src/main/java/com/ecommorce/eservice/EserviceApplication.java
@@ -1,5 +1,6 @@
 package com.ecommorce.eservice;
 
+import com.ecommorce.eservice.model.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,6 @@ public class EserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EserviceApplication.class, args);
     }
+    Customer customer = new Customer();
 
 }
---
+++ src/main/java/com/ecommorce/eservice/model/Customer.java
@@ -0,0 +1,40 @@
+package com.ecommorce.eservice.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+
+@NoArgsConstructor
+@Entity
+@Getter
+@Table(name = "customer_info")
+public class Customer{
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @NotBlank
+    @Column(name = "email")
+    @Getter
+    private String email;
+
+    @NotBlank
+    @Column(name = "password")
+    private String password;
+
+
+    @Column
+    private List<Order> orderHistory;
+
+
+    @Column
+    private Cart userCart;
+}
