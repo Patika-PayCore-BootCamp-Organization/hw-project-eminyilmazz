055ee3bcca2c68489588440524c6b4bc2ab3e953
Emin <eminyilmzz@gmail.com>
2022-02-03 01:22:10 +0300

    AuthenticationService implemented

---
+++ src/main/java/com/ecommorce/eservice/service/AuthenticationService.java
@@ -0,0 +1,8 @@
+package com.ecommorce.eservice.service;
+
+import com.ecommorce.eservice.model.Customer;
+
+public interface AuthenticationService {
+    boolean authenticateToken(String token);
+    public Customer getUser(String token);
+}
