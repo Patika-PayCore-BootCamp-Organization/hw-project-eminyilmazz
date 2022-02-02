2e962bae5705cf5d2ddc554b247b8c3ed10f625a
Emin <eminyilmzz@gmail.com>
2022-02-02 19:17:26 +0300

    Fixed a referencing issue

--- src/main/java/com/ecommorce/eservice/repository/OrderRepository.java
+++ src/main/java/com/ecommorce/eservice/repository/OrderRepository.java
@@ -1,8 +1,9 @@
 package com.ecommorce.eservice.repository;
 
+import com.ecommorce.eservice.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import javax.persistence.criteria.Order;
-
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
